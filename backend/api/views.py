ad208f0451b4aa25afad82f8ca8c5d5f482f63df
dxdelvin <dxdelvin@gmail.com>
2025-03-28 20:32:34 +0100

    Added Frontend Setup with API connection and AUTH checks! and redirection/ Updated Notes for Testing/

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny 
+from .serializers import NoteSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny  
+from .models import Note
 
+class NoteListCreate(generics.CreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]  
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
+    def perform_create(self, serializer):
+        user = self.request.user
+        if serializer.is_valid():
+            serializer.save(author=user)
+        else:
+            print(serializer.errors)
+            
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]  
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+                
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
